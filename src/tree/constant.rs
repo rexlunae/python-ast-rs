7adcd7b959a68d1b6adf4eaa57e6b4aa423b53d4
Erica Stith <rexlunae@gmail.com>
2023-06-25 16:42:06 -0700

    Added type derivations.

--- src/tree/constant.rs
+++ src/tree/constant.rs
@@ -1,6 +1,6 @@
 use pyo3::{PyAny, FromPyObject, PyResult};
 
-#[derive(Clone, Debug, Default, FromPyObject)]
+#[derive(Clone, Debug, Default, FromPyObject, PartialEq)]
 pub struct Constant {
     pub value: String,
 }
