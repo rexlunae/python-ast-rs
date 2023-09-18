675bab213ea944f44f6796ac862aab9235c518e7
Erica Stith <rexlunae@gmail.com>
2023-09-18 10:21:05 -0700

    Added support for dumping lists

--- src/ast_dump.py
+++ src/ast_dump.py
@@ -2,7 +2,10 @@ import ast
 #from io import StringIO
 
 # Read the parse tree
-def ast_dump(s, indent=None):
-    print("Dump:", s)
+def ast_dump(s, indent=None, depth=0):
+    if type(s) == list:
+        l = []
+        for i in s:
+            l.append(ast_dump(i, indent, depth=depth+1))
+        return '[' + ', '.join(l) + ']'
     return ast.dump(s, indent=indent)
-    print("...end")
