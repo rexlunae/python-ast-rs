b85013c350bcf0394c19a16febff80455c61c928
Erica Stith <rexlunae@gmail.com>
2023-10-07 11:18:30 -0700

    Use litrs.

--- src/tree/constant.rs
+++ src/tree/constant.rs
@@ -1,26 +1,34 @@
-//use std::fmt::Display;
 use pyo3::{FromPyObject, PyAny, PyResult};
 use crate::{CodeGen, PythonContext};
-use proc_macro2::{TokenStream};
+use proc_macro2::*;
+use litrs::Literal;
 use quote::{quote};
+use std::fmt::*;
 
-#[derive(Clone, Debug, Default, PartialEq)]
+use log::debug;
+
+#[derive(Clone, Debug)]
 //#[pyo3(transparent)]
-pub struct Constant(pub String);
+pub struct Constant(pub Literal<String>);
 
 impl<'a> FromPyObject<'a> for Constant {
     fn extract(ob: &'a PyAny) -> PyResult<Self> {
-        println!("Constant: {:?} {:?} {:?}", ob, crate::ast_dump(ob, Some(4))?, ob.getattr("value")?);
+        debug!("constant type: {}...{}", ob.get_type().name()?, crate::ast_dump(ob, Some(8))?);
+        debug!("constant value: {:?}", ob.getattr("value")?);
         let v: String = ob.getattr("value")?.extract()?;
-        return Ok(Constant(v))
+        if let Ok(l) = Literal::parse(v) {
+            Ok(Self(l))
+        }
+        else {
+            Err(pyo3::PyErr::from_value(ob))
+        }
+
     }
 }
 
 impl CodeGen for Constant {
-    fn to_rust(self, _ctx: &mut PythonContext) -> Result<TokenStream, Box<dyn std::error::Error>> {
-        let s = self.0.clone();
-        let v :TokenStream = (&s).parse()?;
-        Ok(quote!(#v))
+    fn to_rust(self, _ctx: &mut PythonContext) -> std::result::Result<TokenStream, Box<dyn std::error::Error>> {
+        Ok(quote!(self.0))
     }
 }
 
