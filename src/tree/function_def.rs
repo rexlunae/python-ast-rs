9e611a23cde84ae962069170327c9322fee8c7f0
Erica Stith <rexlunae@gmail.com>
2023-10-07 11:18:52 -0700

    Add docstring.

--- src/tree/function_def.rs
+++ src/tree/function_def.rs
@@ -4,7 +4,7 @@ use quote::{format_ident, quote};
 //use syn::Visibility;
 
 use crate::codegen::{CodeGen, PythonContext};
-use crate::tree::{ParameterList, Statement};
+use crate::tree::{ParameterList, Statement, ExprType};
 
 use log::debug;
 
@@ -16,7 +16,7 @@ pub struct FunctionDef {
     pub decorator_list: Vec<String>,
 }
 
-impl CodeGen for FunctionDef {
+impl<'a> CodeGen for FunctionDef {
     fn to_rust(self, ctx: &mut PythonContext) ->Result<TokenStream, Box<dyn std::error::Error>> {
         let mut streams = TokenStream::new();
         let fn_name = format_ident!("{}", self.name);
@@ -31,14 +31,20 @@ impl CodeGen for FunctionDef {
             format_ident!("pub")
         };
 
-        let parameters = self.args.to_rust(ctx)?;
+        let parameters = self.args.clone().to_rust(ctx)?;
 
         for s in self.body.iter() {
             streams.extend(s.clone().to_rust(ctx)?);
             streams.extend(quote!(;));
         }
 
+        let docstring = if let Some(d) = self.get_docstring() {
+            format!("/// {}", d)
+        } else { "".to_string() };
+
+
         let function = quote!{
+            #docstring
             #visibility fn #fn_name(#parameters) {
                 #streams
             }
@@ -51,5 +57,18 @@ impl CodeGen for FunctionDef {
     // override the default to allow functions to be compiled as trait members.
     fn to_rust_trait_member(&self, ctx: &mut PythonContext) -> Result<TokenStream, Box<dyn std::error::Error>> {
         (*self).clone().to_rust(ctx)
+    }
+
+    fn get_docstring(&self) -> Option<String> {
+        let expr = self.body[0].clone();
+        match expr {
+            Statement::Expr(e) => {
+                match e.value {
+                    ExprType::Constant(c) => Some(c.0.to_string()),
+                    _ => None,
+                }
+            },
+            _ => None,
+        }
     }
 }
