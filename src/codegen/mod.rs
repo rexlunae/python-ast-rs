90d74d161ede22477aa8fda7f778cb516290b4a8
Erica Stith <rexlunae@gmail.com>
2023-10-22 21:36:48 -0700

    Add an error message generator.

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -135,7 +135,7 @@ pub enum CodeGenContext {
 
 /// A trait for AST elements that represent a position in a source file. Implementing this trait allows
 /// an ergonomic means of extracting line and column information from an item.
-pub trait Node {
+pub trait Node<'a> {
     /// A method for getting the starting line number of the node. This may not exist for all node types.
     fn lineno(&self) -> Option<usize> {
         None
@@ -155,9 +155,15 @@ pub trait Node {
     fn end_col_offset(&self) -> Option<usize> {
         None
     }
+
+    /// Generate an error message for the current code, adding line and column number.
+    fn error_message(&self, mod_name: &'a str, message: &'a str) -> String {
+        format!("{} {}:{:?}:{:?}", message, mod_name, self.lineno(), self.col_offset())
+    }
 }
 
-impl Node for PyAny {
+// These will only work on objects of Python's ast library's nodes, but you can try them on anything.
+impl<'a> Node<'a> for PyAny {
     /// A method for getting the starting line number of the node. This may not exist for all node types.
     fn lineno(&self) -> Option<usize> {
         let lineno = self.getattr("lineno");
