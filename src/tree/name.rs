5262bc38bdf1de94ff59fdc1ea6ede3c8bd4f1bc
Erica Stith <rexlunae@gmail.com>
2023-11-08 22:11:50 -0800

    Allow code generation from name.

--- src/tree/name.rs
+++ src/tree/name.rs
@@ -1,7 +1,21 @@
 use pyo3::{FromPyObject};
+use quote::{quote, format_ident};
+use proc_macro2::TokenStream;
+
+use crate::codegen::{CodeGen, PythonOptions, CodeGenContext};
 
 #[derive(Clone, Debug, Default, FromPyObject, PartialEq)]
 //#[pyo3(transparent)]
 pub struct Name {
     pub id: String,
+}
+
+impl<'a> CodeGen for Name {
+    type Context = CodeGenContext;
+    type Options = PythonOptions;
+
+    fn to_rust(self, _ctx: Self::Context, _options: Self::Options) -> Result<TokenStream, Box<dyn std::error::Error>> {
+        let name = format_ident!("{}", self.id);
+        Ok(quote!(#name))
+    }
 }
