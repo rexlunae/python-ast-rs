1b8813a80d3b852058d708bd8b5b955da5486642
Erica Stith <rexlunae@gmail.com>
2023-12-22 19:32:22 -0800

    Added symbols into library.

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,13 @@ pub use codegen::*;
 pub mod scope;
 pub use scope::*;
 
+pub mod symbols;
+pub use symbols::*;
+
 use pyo3::prelude::*;
 use std::include_str;
+
+pub mod pytypes;
 
 pub use pyo3::PyResult;
 
@@ -29,10 +34,35 @@ pub fn parse<'a>(input: &'a str, filename: &str) -> PyResult<tree::Module> {
         let args = (input, filename);
 
         let tree: tree::Module = t.call1(args)?.extract()?;
+        //let tree: tree::Module = pythonize::depythonize(t.call1(args.clone())?).unwrap();
+        //println!("test_tree: {:?}", tree);
 
         Ok(tree)
     })
 }
+
+/// Takes a string of bytes and returns the Python-tokenized version of it.
+pub fn parse_with_serde<'a>(input: &'a str, filename: &str) -> PyResult<tree::Module> {
+
+    let pymodule_code = include_str!("parser.py");
+
+    Python::with_gil(|py| -> PyResult<Module> {
+        // We want to call tokenize.tokenize from Python.
+        let pymodule = PyModule::from_code(py, pymodule_code, "parser.py", "parser")?;
+        let t = pymodule.getattr("parse")?;
+        assert!(t.is_callable());
+        let args = (input, filename);
+
+        let pytree = t.call1(args)?;
+        println!("::::::::::::::::::::::{}", ast_dump(&pytree, None)?);
+
+        let tree: tree::Module = pythonize::depythonize(pytree)?;
+        println!("::::::::::::::::::::tree: {:?}", tree);
+
+        Ok(tree)
+    })
+}
+
 
 /// Accepts any Python object and dumps it using the Python ast module.
 pub fn ast_dump(o: &PyAny, indent: Option<u8>) -> PyResult<String> {
@@ -64,34 +94,4 @@ pub fn sys_path() -> PyResult<Vec<String>> {
 
         Ok(paths)
     })
-}
-
-#[cfg(test)]
-mod tests {
-    //use super::*;
-
-    /*
-    #[test]
-    fn check_token_stream() {
-        let result = parse("#test comment
-def foo():
-    pass
-", "test_case").unwrap();/*
-        println!("tokens: {:?}", result);
-        assert_eq!(result[0].token_text, "COMMENT");
-        assert_eq!(result[1].token_text, "NL");
-        assert_eq!(result[2].token_text, "NAME");
-        assert_eq!(result[3].token_text, "NAME");
-        assert_eq!(result[4].token_text, "OP");
-        assert_eq!(result[5].token_text, "OP");
-        assert_eq!(result[6].token_text, "OP");
-        assert_eq!(result[7].token_text, "NEWLINE");
-        assert_eq!(result[8].token_text, "INDENT");
-        assert_eq!(result[9].token_text, "NAME");
-        assert_eq!(result[10].token_text, "NEWLINE");
-        assert_eq!(result[11].token_text, "DEDENT");
-        assert_eq!(result[12].token_text, "ENDMARKER");*/
-        info!("{:?}", result);
-    }*/
-
 }
