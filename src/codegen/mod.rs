f761a71ed4f243c760dc0ef2f773ce309926c6dc
Erica Stith <rexlunae@gmail.com>
2024-01-10 20:11:11 -0800

    Removed some unused code.

--- src/codegen/mod.rs
+++ src/codegen/mod.rs
@@ -58,54 +58,6 @@ impl Default for PythonOptions {
     }
 }
 
-impl PythonOptions {
-
-    /// Scans the Python path for the short name given, and returns the full path. Note that it only searches
-    /// for the path itself, not any subpath.
-    pub fn search_path<S: Into<String> + Clone + Ord + Borrow<S>>(&self, file: S) -> Result<String, Box<dyn std::error::Error>> {
-        for entry in self.python_path.clone() {
-            let path_string = format!("{}{}{}", entry, MAIN_SEPARATOR, file.clone().into());
-            if Path::new(path_string.as_str()).exists() {
-                return Ok(path_string)
-            }
-        }
-        let error = CodeGenError::PathNotFound(file.into());
-        Err(error.into())
-    }
-
-    /// Searches the Python path for the module and returns its contents.
-    pub fn load<S: Into<String> + Clone + Ord + Borrow<S>>(&self, module: S) -> Result<String, Box<dyn std::error::Error>> {
-        let module_string:String = module.into();
-        let module_parts: Vec<&str> = module_string.split('.').collect();
-        let module_path = if module_parts.len() == 1 {
-            self.search_path(format!("{}.py", module_parts[0]))?
-        } else {
-            let _first = self.search_path(module_parts[0]);
-            format!("{}.py", module_parts[1..].join(format!("{}", MAIN_SEPARATOR).as_str()))
-        };
-
-        let mut file = File::open(&module_path)?;
-        let mut s = String::new();
-
-        file.read_to_string(&mut s)?;
-        Ok(s)
-    }
-
-
-    pub fn import<S: Into<String> + Clone + Ord + Borrow<S>>(&mut self, from: S, to: S) {
-        let f: String = from.into();
-        let t: String = to.into();
-
-        if !self.imports.contains_key(&f.clone()) {
-            self.imports.insert(f.clone(), HashSet::new());
-        }
-        if let Some(m) = self.imports.get_mut(&f.clone()) {
-            m.insert(t);
-        }
-    }
-}
-
-
 /// Reexport the CodeGen from to_tokenstream
 pub use to_tokenstream::CodeGen;
 
