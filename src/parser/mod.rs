3ac17a1f599579f8a666ac33a9f25f67e1a3edb6
Erica Stith <rexlunae@gmail.com>
2024-02-18 23:31:18 -0800

    Split parse() into two functions.

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,6 +2,26 @@ use crate::{dump, Module};
 
 use pyo3::prelude::*;
 
+use serde_pyobject::*;
+
+/// Takes a string of Python code and emits a Python struct that represents the AST.
+fn parse_to_py(input: impl AsRef<str>, filename: impl AsRef<str>, py: Python<'_>) -> PyResult<PyObject> {
+
+    let pymodule_code = include_str!("__init__.py");
+
+    // We want to call tokenize.tokenize from Python.
+    let pymodule = PyModule::from_code(py, pymodule_code, "__init__.py", "parser")?;
+    let t = pymodule.getattr("parse")?;
+    assert!(t.is_callable());
+    let args = (input.as_ref(), filename.as_ref());
+
+    let py_tree = t.call1(args)?;
+    log::debug!("py_tree: {}", dump(py_tree, Some(4))?);
+
+    Ok(py_tree.into())
+}
+
+
 /// Takes a string of bytes and returns the Python-tokenized version of it.
 /// use python_ast::parse;
 ///
@@ -14,21 +34,10 @@ use pyo3::prelude::*;
 ///}
 /// ```
 pub fn parse(input: impl AsRef<str>, filename: impl AsRef<str>) -> PyResult<Module> {
+    let module = Python::with_gil(|py| {
+        let py_tree = parse_to_py(input, filename, py)?;
+        py_tree.extract(py)
+    });
 
-    let pymodule_code = include_str!("__init__.py");
-
-    Python::with_gil(|py| -> PyResult<Module> {
-        // We want to call tokenize.tokenize from Python.
-        let pymodule = PyModule::from_code(py, pymodule_code, "__init__.py", "parser")?;
-        let t = pymodule.getattr("parse")?;
-        assert!(t.is_callable());
-        let args = (input.as_ref(), filename.as_ref());
-
-        let py_tree = t.call1(args)?;
-        log::debug!("py_tree: {}", dump(py_tree, Some(4))?);
-
-        let tree: Module = py_tree.extract()?;
-
-        Ok(tree)
-    })
+    Ok(module?)
 }
