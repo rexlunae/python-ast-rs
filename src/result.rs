189c2784e344a6017f6ba955b759eda0d676ba96
Erica Stith <rexlunae@gmail.com>
2024-05-12 22:48:19 -0700

    Added additional error types.

--- src/result.rs
+++ src/result.rs
@@ -1,20 +1,43 @@
 use std::borrow::Borrow;
 
-use thiserror::{Error};
+use thiserror::Error as E;
 use pyo3::PyErr;
 
-#[derive(Error, Debug)]
-pub enum CodeGenError<S: Into<String> + Clone + Ord + Borrow<S>> {
-    #[error("searching path {0} failed")]
-    PathNotFound(S),
-    #[error("Not yet implemented: {0}")]
-    NotYetImplemented(S),
+use crate::{
+    BinOp, BoolOp, Compare, Expr, ExprType, StatementType, UnaryOp,
+};
+
+#[derive(E, Debug)]
+pub enum Error {
+    #[error("BinOp type not yet implemented: {:?}", .0)]
+    BinOpNotYetImplemented(BinOp),
+
+    #[error("BoolOp type not yet implemented: {:?}", .0)]
+    BoolOpNotYetImplemented(BoolOp),
+
+    #[error("Compare type not yet implemented: {:?}", .0)]
+    CompareNotYetImplemented(Compare),
+
+    #[error("Expr type not yet implemented: {:?}", .0)]
+    ExprNotYetImplemented(Expr),
+    #[error("ExprType type not yet implemented: {:?}", .0)]
+    ExprTypeNotYetImplemented(ExprType),
+
     #[error("Unknown type {0}")]
-    UnknownType(S),
+    UnknownType(String),
 
     #[error("PyO3 Error: {0}")]
     #[from(PyErr)]
     Pyo3Error(PyErr),
+
+    #[error("Statement type not yet implemented: {:?}", .0)]
+    StatementNotYetImplemented(StatementType),
+    #[error("UnaryOp type not yet implemented: {:?}", .0)]
+    UnaryOpNotYetImplemented(UnaryOp),
+
+    #[error("Unknown Error: {0}")]
+    #[from(Box<dyn std::error::Error>)]
+    UnknownError(Box<dyn std::error::Error>)
 }
 
-pub type Result<T, S> = std::result::Result<T, CodeGenError<S>>;
+pub type Result<T> = std::result::Result<T, Error>;
