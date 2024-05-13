341443e3d450d39a4090cdfd6aa4cc645d2b3927
Erica Stith <rexlunae@gmail.com>
2024-05-12 18:01:20 -0700

    Add PyO3 error type.

--- src/result.rs
+++ src/result.rs
@@ -1,6 +1,7 @@
 use std::borrow::Borrow;
 
-use thiserror::Error;
+use thiserror::{Error};
+use pyo3::PyErr;
 
 #[derive(Error, Debug)]
 pub enum CodeGenError<S: Into<String> + Clone + Ord + Borrow<S>> {
@@ -10,6 +11,10 @@ pub enum CodeGenError<S: Into<String> + Clone + Ord + Borrow<S>> {
     NotYetImplemented(S),
     #[error("Unknown type {0}")]
     UnknownType(S),
+
+    #[error("PyO3 Error: {0}")]
+    #[from(PyErr)]
+    Pyo3Error(PyErr),
 }
 
 pub type Result<T, S> = std::result::Result<T, CodeGenError<S>>;
